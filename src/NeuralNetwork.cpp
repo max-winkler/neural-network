cdf7fa6401455f220650b6a5b0680ed7faa2c496
Max Winkler <max.winkler@mathematik.tu-chemnitz.de>
2023-07-14 13:18:34 +0200

    Neural network class and test program added

---
+++ src/NeuralNetwork.cpp
@@ -0,0 +1,70 @@
+#include "NeuralNetwork.h"
+
+NeuralNetwork::NeuralNetwork(std::vector<size_t> width_) : width(width_),
+							   weight(), bias(), activation(),
+							   layers(width_.size())
+{
+  // Add output dimension
+  width.push_back(1);
+
+  weight.reserve(layers);
+  bias.reserve(layers);
+  activation.reserve(layers);
+  
+  for(Dimension::const_iterator it = width.begin(); it+1 != width.end(); ++it)
+    {
+      weight.push_back(Matrix(*(it+1), *it));
+      bias.push_back(Vector(*(it+1)));
+    }
+}
+
+void NeuralNetwork::setParameters(size_t layer, const Matrix& matrix, const Vector& vector, ActivationFunction act)
+{
+  if(layer < 0 || layer > layers-1)
+    {
+      std::cerr << "Error: The layer " << layer << " is not feasible. Must be between 0 and " << layers-1 << std::endl;
+      return;
+    }
+
+  if(matrix.nRows() != width[layer+1] || matrix.nCols() != width[layer])
+    {
+      std::cerr << "Error: The provided weight matrix has invalid size.\n";
+      std::cerr << "  Given is (" << matrix.nRows() << "," << matrix.nCols() << ")"
+		<< " but required is (" << width[layer+1] << "," << width[layer] << ")\n";
+      return;
+    }
+  
+  if(vector.size() != width[layer+1])
+    {
+      std::cerr << "Error: The provided bias vector has invalid size.\n";
+      std::cerr << "  Given is (" << vector.size() 
+		<< " but required is (" << width[layer+1] << "," << ")\n";
+      return;
+    }
+  
+  weight[layer] = matrix;
+  bias[layer] = vector;
+  activation[layer] = act;
+}
+
+double NeuralNetwork::eval(const Vector& x) const
+{
+  Vector x_tmp(x);
+  for(size_t l=0; l<layers; ++l)    
+    x_tmp = activate(weight[l] * x_tmp + bias[l], activation[l]);
+
+  return x_tmp[0];
+}
+
+std::ostream& operator<<(std::ostream& os, const NeuralNetwork& net) 
+{
+  os << "Neural network with (" << (net.layers) << " layers)\n";
+  os << "  Input dimension         : " << (net.width[0]) << "\n";
+
+  os << "  Hidden layer dimensions : ";
+  for(size_t i=1; i<net.layers-1; ++i)    
+    os << net.width[i] << ", ";
+  os << net.width[net.layers-1] << std::endl;
+  
+  return os;
+}
---
+++ test/NeuralNetwork.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+#include "NeuralNetwork.h"
+
+int main()
+{
+  Dimension layers;
+  layers.push_back(2); // input layer
+  layers.push_back(3); // hidden layer
+  
+  NeuralNetwork net(layers);
+
+  Matrix W1(3,2);
+  Vector B1(3);
+
+  W1 = {1.2139, 0.9372, 2.3817, -0.1927, -1.2983, 3.2739};
+  B1 = {0.2397, 1.9104, -4.8739};
+  
+  net.setParameters(0, W1, B1, ActivationFunction::SIGMOID);
+
+  Matrix W2(1,3);
+  Vector B2(1);
+
+  W2 = {-2.3981, 1.3791};
+  B2 = {1.6378};
+
+  net.setParameters(1, W2, B2, ActivationFunction::NONE);
+
+  std::cout << net;
+
+  Vector x(2);
+  x = {0.8, -0.5};
+  double res = net.eval(x);
+
+  std::cout << "Evaluation: " << res << std::endl;
+  return 0;
+}
