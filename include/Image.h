f97647bd1a2c25f13169df51ce0eccbc49940e92
Max Winkler <max.winkler@mathematik.tu-chemnitz.de>
2025-05-16 13:55:59 +0200

    Working on design of an image class

--- include/Image.h
+++ include/Image.h
@@ -3,11 +3,19 @@
 
 #include <png.h>
 #include <string>
+#include <vector>
 
 class Image
 {
  public:
   static void write(const std::string& filename, int width, int height, unsigned char** row_pointers);
+  static Image read(const std::string& filename);
+ private:
+  std::vector<unsigned char> pixels;
+  size_t width;
+  size_t height;
+  int color_type;
+  int bit_depth;
 };
 
 #endif
--- src/Image.cpp
+++ src/Image.cpp
@@ -2,6 +2,61 @@
 
 #include <iostream>
 
+std::vector<unsigned char> Image::read(const std::string& filename, int& width, int& height,
+                                       int& color_type, int& bit_depth)
+{
+  FILE *fp = fopen(filename.c_str(), "rb");
+  
+  png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  png_infop info = png_create_info_struct(png);
+  
+  png_init_io(png, fp);
+  png_read_info(png, info);
+
+  width      = png_get_image_width(png, info);
+  height     = png_get_image_height(png, info);
+  color_type = png_get_color_type(png, info);
+  bit_depth  = png_get_bit_depth(png, info);
+  
+  if(bit_depth == 16)
+    png_set_strip_16(png);
+
+  if(color_type == PNG_COLOR_TYPE_PALETTE)
+    png_set_palette_to_rgb(png);
+
+  // PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
+  if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+    png_set_expand_gray_1_2_4_to_8(png);
+
+  if(png_get_valid(png, info, PNG_INFO_tRNS))
+    png_set_tRNS_to_alpha(png);
+
+  // These color_type don't have an alpha channel then fill it with 0xff.
+  if(color_type == PNG_COLOR_TYPE_RGB ||
+     color_type == PNG_COLOR_TYPE_GRAY ||
+     color_type == PNG_COLOR_TYPE_PALETTE)
+    png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+
+  if(color_type == PNG_COLOR_TYPE_GRAY ||
+     color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+    png_set_gray_to_rgb(png);
+
+  png_read_update_info(png, info);
+
+  png_bytep* row_pointers = new png_bytep[height];
+  for(int y = 0; y < height; y++) {
+    row_pointers[y] = new png_byte[png_get_rowbytes(png,info)];
+  }
+
+  png_read_image(png, row_pointers);
+
+  fclose(fp);
+
+  png_destroy_read_struct(&png, &info, NULL);
+
+  return row_pointers;
+}
+
 void Image::write(const std::string& filename, int width, int height, unsigned char** row_pointers)
 {
   // Write image file
@@ -31,5 +86,3 @@ void Image::write(const std::string& filename, int width, int height, unsigned c
     png_destroy_write_struct(&png, &info);
 
 }
-
-
